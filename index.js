afaa9d89caf4358cf7787214ba209ac6050f566c
Joseph Stewart <stewartjoseph255@gmail.com>
2023-04-11 15:32:52 -0600

    Javascript Functions Continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return (`This Saturday, I want to ${fun}!`);
+}
+saturdayFun();
+
+function mondayWork(work = 'go to the office') {
+    return (`This Monday, I will ${work}.`)
+}
+
+mondayWork();
+
+function wrapAdjective(character = '*') {
+    const compliment = function(special = 'special') {
+        return "You are " + character + special + character + "!";
+    };
+    return compliment;
+}
